49034f07076567902cda5d820aa85c88fafdbd8c
Natufik <47058113+Natufik@users.noreply.github.com>
2021-01-31 19:53:18 +0200

    Task24 - Homework by Nat.Soshko

---
+++ Task24.java
@@ -0,0 +1,33 @@
+package com.alpha.work24;
+
+import java.util.Scanner;
+
+public class Task24 {
+    public static void main(String[] args) {
+        System.out.print("Enter the 3-digit number: ");
+        Scanner sc = new Scanner(System.in);
+        int iNum = sc.nextInt();
+        while (iNum < 100 || iNum >= 1000) {
+            System.out.println("Your number isn't 3-digit. Enter the 3-digit number:");
+            iNum = sc.nextInt();
+        }
+        //System.out.println ("Before reverse the number = " + iNum);
+        System.out.print ("After reverse the number = ");
+        System.out.print(iNum % 10);
+        iNum = iNum / 10;
+        System.out.print(iNum % 10);
+        iNum = iNum / 10;
+        System.out.println(iNum % 10);
+
+        /*// using cycle
+        System.out.print("Enter the 3-digit number: ");
+        Scanner sc2 = new Scanner(System.in);
+        int iNum2 = sc2.nextInt();
+        System.out.print ("After reverse the number = ");
+        System.out.print(iNum2 % 10);
+        while ( (iNum2 /= 10) != 0) {
+            System.out.print(iNum2 % 10);
+        }
+        */
+    }
+}
